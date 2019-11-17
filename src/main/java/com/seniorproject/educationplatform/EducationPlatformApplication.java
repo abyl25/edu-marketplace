aa170040c23b9540c660a517deeb2f531982177e
abyl25 <abylay.tastanbekov@nu.edu.kz>
2019-11-18 00:42:34 +0600

    commit

--- src/main/java/com/seniorproject/educationplatform/EducationPlatformApplication.java
+++ src/main/java/com/seniorproject/educationplatform/EducationPlatformApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication // (exclude={DataSourceAutoConfiguration.class})
@@ -28,6 +33,13 @@ class HelloController {
     @GetMapping("/")
     public String hello() {
         return "Hello there!";
+    }
+
+    @PostMapping("/")
+    public ResponseEntity postMain() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        return ResponseEntity.ok().headers(responseHeaders).body("Response with header using ResponseEntity");
     }
 
 }
---
+++ src/main/java/com/seniorproject/educationplatform/filters/AddResponseHeaderFilter.java
@@ -0,0 +1,41 @@
+package com.seniorproject.educationplatform.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+@Component
+public class AddResponseHeaderFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("LOG: AddResponseHeaderFilter: filtering response");
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+
+        if (httpServletRequest.getMethod().equalsIgnoreCase("OPTIONS")) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
